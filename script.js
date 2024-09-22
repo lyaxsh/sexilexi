cc1777f4254ec2ce2c9560ff01970dfda43c175f
Laksh Nahar <laksh14008@gmail.com>
2024-09-22 09:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,79 @@
 // script.js
-document.addEventListener('mousemove', (e) => {
-    const background = document.querySelector('.background');
-    
-    // Calculate the mouse position relative to the window width and height
-    let xPos = (e.clientX / window.innerWidth) - 0.5;
-    let yPos = (e.clientY / window.innerHeight) - 0.5;
-    
-    // Move the background slightly based on the mouse position
-    background.style.transform = `translate(${xPos * 50}px, ${yPos * 50}px) rotate(${xPos * 10}deg)`;
+particlesJS('particles-js',
+{
+    "particles": {
+        "number": {
+            "value": 80, // Number of particles
+            "density": {
+                "enable": true,
+                "value_area": 800 // Density of particles
+            }
+        },
+        "color": {
+            "value": "#61dafb" // Particle color
+        },
+        "shape": {
+            "type": "circle", // Particle shape
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150, // Distance between particles
+            "color": "#61dafb", // Line color
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6, // Speed of particle movement
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab" // Particles grab on hover
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push" // Particles push on click
+            },
+            "resize": true
+        }
+    },
+    "retina_detect": true
 });
